11e0d0a0efd26d7e8098e1601ace5098f4010a54
Eric Quintana Muñoz <71662908+SinkorAzon@users.noreply.github.com>
2021-05-28 15:35:58 +0200

    Update constants.js

--- js/constants.js
+++ js/constants.js
@@ -22,7 +22,7 @@ var myshared_secret="dff45169a9bd10061e6f7313a0595509";
  * 
  */
 function myLoginFunction(){
-  var url= 'http://www.last.fm/api/auth/?api_key=6639a92311bbbc06dd40a075be240e27&cb=http://localhost:3000/mainpage.html';
+  var url= 'http://www.last.fm/api/auth/?api_key=6639a92311bbbc06dd40a075be240e27&cb=https://sinkorazon.github.io/PractLastFm/mainpage.html';
 
   window.location.replace(url);
 }
